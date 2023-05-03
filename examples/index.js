c2d08ffd7f95c97a04df6b1ee4acb1e4467750e9
Axel Rothe <a.rothe@vanrothe.com>
2023-05-03 21:41:54 +0200

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -24,7 +24,7 @@ wapi.signIn("admin", "admin").then((token) => {
 	wapi.connect().then((client) => {
 		if (!client) throw new Error("Failed to connect");
 
-		wapi.query.library.one('test').fetch()then((library) => {
+		wapi.query.library.one('test').fetch().then((library) => {
 			console.log(library);
 		})
 	});
